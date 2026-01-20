09e1f5d53cda480c9209c0cc52bdf097386b12a3
artem_qwverh <artem.kuzmin281@gmail.com>
2026-01-20 09:36:33 +0300

    first sem

---
+++ first_sem/src/main/java/controller/WelcomeServlet.java
@@ -0,0 +1,22 @@
+package controller;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/", "/welcome"})
+public class WelcomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        session.setAttribute("currentContext", req.getContextPath());
+        req.getRequestDispatcher("/start.ftlh").forward(req, resp);
+    }
+}
